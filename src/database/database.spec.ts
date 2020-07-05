2c6aa755795a9027e3d1763de33ffbffcaa7df32
davezabriskie <42191246+davezabriskie@users.noreply.github.com>
2020-07-05 12:49:27 -0400

    Add test for database disconnect

--- src/database/database.spec.ts
+++ src/database/database.spec.ts
@@ -3,24 +3,59 @@ import { logger } from '../utils/logger';
 
 jest.mock('mongoose');
 
-describe('connect', () => {
+describe('Database functions', () => {
   const dbModule = require('./database');
   const uri = process.env.MONGODB_URI;
+  let logInfoSpy: jasmine.Spy;
+  let logErrorSpy: jasmine.Spy;
 
-  it('should connect database succesfully', done => {
-    const loggerSpyOn = jest.spyOn(logger, 'info');
-    const mongooseConnectSpyOn = jest
-      .spyOn<Mongoose, 'connect'>(mongoose, 'connect')
-      .mockImplementationOnce((uris: string, options?: ConnectionOptions, callback?: (err?: Error) => void) => {
-        if (callback) {
-          callback();
-          done();
-        }
-        return Promise.resolve(mongoose);
-      });
+  beforeEach(() => {
+    logInfoSpy = spyOn(logger, 'info');
+    logErrorSpy = spyOn(logger, 'error');
+  });
 
-    dbModule.connect();
-    expect(mongooseConnectSpyOn).toBeCalledWith(
+  describe('connect', () => {
+    let mongooseConnectSpyOn: jasmine.Spy;
+
+    beforeEach(() => {
+      mongooseConnectSpyOn = spyOn(mongoose, 'connect');
+    });
+
+    it('should connect database succesfully', () => {
+      mongooseConnectSpyOn.and
+        .callFake((uris: string, options?: ConnectionOptions, callback?: (err?: Error) => void) => {
+          if (callback) {
+            callback();
+          }
+          return Promise.resolve(mongoose);
+        });
+
+      dbModule.connect();
+      expect(mongooseConnectSpyOn).toBeCalledWith(
+          uri,
+          {
+            useNewUrlParser: true,
+            useFindAndModify: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+          },
+        dbModule.callback
+      );
+      expect(logInfoSpy).toBeCalledWith('Connected to database');
+      expect(logErrorSpy).not.toHaveBeenCalled();
+    });
+
+    it('connection error', () => {
+      mongooseConnectSpyOn.and
+        .callFake((uris: string, options?: ConnectionOptions, callback?: (err?: Error) => void) => {
+          if (callback) {
+            callback(new Error('Error connecting to database'));
+          }
+          return Promise.resolve(mongoose);
+        });
+
+      dbModule.connect();
+      expect(mongooseConnectSpyOn).toBeCalledWith(
         uri,
         {
           useNewUrlParser: true,
@@ -28,36 +63,20 @@ describe('connect', () => {
           useUnifiedTopology: true,
           useCreateIndex: true,
         },
-      dbModule.callback
-    );
-    expect(loggerSpyOn).toBeCalledWith('Connected to database');
-    loggerSpyOn.mockRestore();
+        dbModule.callback
+      );
+      expect(logInfoSpy).not.toHaveBeenCalled();
+      expect(logErrorSpy).toBeCalledWith('Error connecting to database');
+    });
   });
 
-  it('connection error', done => {
-    const loggerSpyOn = jest.spyOn(logger, 'error');
-    const mongooseConnectSpyOn = jest
-      .spyOn<Mongoose, 'connect'>(mongoose, 'connect')
-      .mockImplementationOnce((uris: string, options?: ConnectionOptions, callback?: (err?: Error) => void) => {
-        if (callback) {
-            callback(new Error('Error connecting to database'));
-          done();
-        }
-        return Promise.resolve(mongoose);
-      });
+  describe('disconnect', () => {
+    const mongooseDisconnectSpyOn = jest
+      .spyOn<Mongoose, 'disconnect'>(mongoose, 'disconnect');
 
-    dbModule.connect();
-    expect(mongooseConnectSpyOn).toBeCalledWith(
-      uri,
-      {
-        useNewUrlParser: true,
-        useFindAndModify: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      },
-      dbModule.callback
-    );
-    expect(loggerSpyOn).toBeCalledWith('Error connecting to database');
-    loggerSpyOn.mockRestore();
+    it('should disconnect', () => {
+      dbModule.disconnect();
+      expect(mongooseDisconnectSpyOn).toHaveBeenCalled();
+    });
   });
 });
--- src/services/mongodb.service.spec.ts
+++ src/services/mongodb.service.spec.ts
@@ -1,28 +1,57 @@
 import { MongoDBService } from './mongodb.service';
 import { logger } from '../utils/logger';
 import { Review } from '../interfaces/review.interface';
-import { connect, disconnect } from '../utils/mongoMemoryMock';
+import { ReviewModel } from '../database/reviews/reviews.model';
 
 describe('MongoDBService', () => {
     let service: MongoDBService;
-    beforeAll(async () => await connect());
-    afterAll(async () => await disconnect());
+    let reviewCreateSpy: jasmine.Spy;
+    let logInfoSpy: jasmine.Spy;
+    let logErrorSpy: jasmine.Spy;
+    const database = require('../database/database');
 
     beforeEach(() => {
         service = new MongoDBService();
+
+        reviewCreateSpy = spyOn(ReviewModel, 'create');
+        logInfoSpy = spyOn(logger, 'info');
+        logErrorSpy = spyOn(logger, 'error');
+        spyOn(database, 'connect');
+        spyOn(database, 'disconnect');
+    });
+
+    describe('connectDB', () => {
+        it('should call to connect', (done) => {
+            service.connectDB().then(() => {
+                expect(database.connect).toHaveBeenCalled();
+                done();
+            });
+        });
     });
 
     describe('populateDB', () => {
-        test('should log document added', async () => {
-            const loggerSpyOn = jest.spyOn(logger, 'info');
-            const review = {
-                artist: 'Danny Brown',
-                album: 'uknowhatimsayin¿',
-            } as Review;
+        const review = {
+            artist: 'Danny Brown',
+            album: 'uknowhatimsayin¿',
+        } as Review;
+        const snippets = [review];
 
-            const snippets = [review];
-            await service.populateDB(snippets);
-            expect(loggerSpyOn).toBeCalledWith('Created review Danny Brown uknowhatimsayin¿');
+        test('should log document added', (done) => {
+            service.populateDB(snippets).then(() => {
+                expect(logInfoSpy).toBeCalledWith('Created review Danny Brown uknowhatimsayin¿');
+                expect(database.disconnect).toHaveBeenCalled();
+                done();
+            });
+        });
+
+        test('should log document added', (done) => {
+            reviewCreateSpy.and.throwError('oops');
+            service.populateDB(snippets).then(() => {
+                expect(logInfoSpy).not.toHaveBeenCalled();
+                expect(logErrorSpy).toHaveBeenCalled();
+                expect(database.disconnect).toHaveBeenCalled();
+                done();
+            });
         });
     });
 });
